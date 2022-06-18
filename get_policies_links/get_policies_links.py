048d53d522fc328084a3ea1f03ad2fa33682b827
Jesus R. Rijo <jesusrrcinco@gmail.com>
2022-06-18 14:53:29 -0400

    Working on links getter.

--- get_policies_links/get_policies_links.py
+++ get_policies_links/get_policies_links.py
@@ -5,36 +5,49 @@
 # Author:      Jesus Rafael Rijo Candelario (Mercer University)
 # Date:        06/15/2022
 # Advisor:     Dr. Lydia Ray.
-# Description: Python script to extract potential IoT companies' privacy policies from
-#              the names text file.
+# Description: Python script to extract links for potential IoT companies' privacy policies 
+#              from the names text file. In order to store the links to a text file, please
+#              pipe the standard output in this fashion:
+#
+#                python3 get_policies_links.py > links.txt 
 #
 #########################################################################################
 
 # Importing libraries.
-import urllib.request
 import time
-from bs4 import BeautifulSoup
+import requests
+import urllib
+from googlesearch import search
 
-for i in range(102):
-  # Getting HTML document, using the index i to explore each page.
-  link = 'http://iotone.com/suppliers?page='+str(i)
-  page = urllib.request.urlopen(link)
-  pageFinal = BeautifulSoup(page, 'html.parser')
+#########################################################################################
+#
+# Function #1
+#
+# Pre: Takes the name of a company.
+# Pos: Outputs the company's name and a link to their privacy policy.
+#
+#########################################################################################
 
-  # Converting HTML content into string and splitting based
-  # on newlines.
-  pageContent = str(pageFinal) 
-  pageContent = pageContent.split('\n')
+def searchPolicy( companyName ):
+  query = companyName + " privacy policy"
+  for x in search(query, tld="co.in", num=1, stop=1, pause=2):
+    print(companyName+" "+x)
 
-  # Searching for the company names and removing surrounding
-  # clutter. Particularly, we want to remove all the information
-  # of the </a> tag EXCEPT for the company's name.
-  for x in pageContent:    
-      if "product-name" in x:
-        x = x[x.find(">")+1:]
-        x = x.replace("</a>", "")
-        print(x)
+#########################################################################################
+#
+# Main function.
+#
+#########################################################################################
 
-  # Sleeping for a few seconds as to not encourage iotone.com to
-  # block our machine's IP.
-  time.sleep(5.0)
+# Opening file with names and reading the names.
+fp    = open("iot_names_3", "r")
+names = fp.readlines()
+
+# Stripping any newlines or extra spaces from the names.
+for i in range(0,len(names)):
+  names[i] = names[i].strip()
+
+for x in range(0,len(names)):
+  searchPolicy(names[x])          # For every 100 requests, we sleep for one minute, as
+  if( x + 1 % 100 == 0 ):         # to not overload Google with too many requests.
+    time.sleep(60.0)
