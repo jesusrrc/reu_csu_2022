31a2bee63f581a41f3edece434fcadbfb85f74ea
Jesus R. Rijo <jesusrrcinco@gmail.com>
2022-06-18 21:49:17 -0400

    Getting raw policies.

--- get_policies/get_policies.py
+++ get_policies/get_policies.py
@@ -6,19 +6,27 @@
 # Date:        06/16/2022
 # Advisor:     Dr. Lydia Ray.
 # Description: Python script used to get the raw HTML files of privacy policies into the
-#              raw policies folder.
+#              raw policies folder. We moved all the produced files into the raw_policies
+#              folder.
 #
 #########################################################################################
 
 # Used libraries.
+import requests
+import sys
 import urllib.request
 import time
 import re
 from bs4 import BeautifulSoup
 
+# Creating an useragent for certain servers that reject
+# requests without user agents.
+header = {
+  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+}
+
 # Arrays to store companies and their links.
-companies = list()
-links =     list()
+pairs = list()
 
 # Opening file with links.
 fp = open("links_7", "r")
@@ -27,18 +35,26 @@ for pair in fp:
   pair = pair.split( "http" )   # Using the "http" token to split
   pair[1] = "http"+pair[1]      # and adding it back.
 
-  pair[0] = pair[0].strip()     # Stripping any newlines.
-  pair[1] = pair[1].strip()
-  
-  companies.append(pair[0])     # Adding each name and link to 
-  links.append(pair[1])         # the right arrays.
+  pair[0] = pair[0].strip()     # Stripping any newlines
+  pair[1] = pair[1].strip()     # and adding pairs to list.
+  pairs.append( pair )
 
-for link in links:
-  page = urllib.request.urlopen(link)           # Making request to page.
-  page = BeautifulSoup(page, 'lxml')
+fp.close()                      # Closing first reader.
 
-  pageContent = str(page)                       # Getting content as string.
+for pair in pairs:
+  req = requests.get(pair[1], headers=header)
+  content = BeautifulSoup(req.content, 'html.parser')
 
-  fp = open( link+".txt", "x" )                 # Writing content to a custom file
-  fp.write( pageContent )                       # and sleeping for a second.
-  time.sleep(1.0)
+  print( content ) 
+
+
+#  page = urllib.request.Request(pair[1])         # Making request to page.
+#  page.add_header("User-Agent", "aUserAgent")
+#  page = urllib.request.urlopen(pair[1]).read()
+  #page = BeautifulSoup(page, 'html.parser')
+  print("done")
+
+#  pageContent = str(page)                         # Getting content as string.
+
+  fp = open( pair[0]+".txt", 'x' )                # Writing content to a custom file
+  fp.write(pageContent)
